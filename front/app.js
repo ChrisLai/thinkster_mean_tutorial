d5072976b02a5b9dcabe27bbf7ccbc4dfd6681be
ChrisLai <chrislai@vt.edu>
2015-01-26 15:59:57 -0500

    Rolled back

--- front/app.js
+++ front/app.js
@@ -1,45 +1,31 @@
-angular.module('flapperNews', ['ui.router'])
-.config([
-	'$stateProvider',
-	'$urlRouter',
-	function($stateProvider, $urlRouter) {
+var flapperNews = angular.module('flapperNews', ['ui.router']);
 
-	  $stateProvider
-	    .state('home', {
-	      url: '/home',
-	      templateUrl: '/home.html',
-	      controller: 'MainCtrl'
-	    });
 
-	  $urlRouter.otherwise('home');
-}])
-.factory('posts', [function(){
+flapperNews.factory('posts', [function(){
   // service body
   var o = {
   	posts: []
   };
   return o;
-}])
-.controller('MainCtrl', [
-	'$scope',
-	'$posts',
+}]);
+
+
+flapperNews.controller('MainCtrl', [
 	function($scope){
 	  	$scope.test = 'Hello world!';
 
-	  	$scope.posts = posts.posts;
+	  	$scope.posts = [
+		  {title: 'post 1', upvotes: 5},
+		  {title: 'post 2', upvotes: 2},
+		  {title: 'post 3', upvotes: 15},
+		  {title: 'post 4', upvotes: 9},
+		  {title: 'post 5', upvotes: 4}
+		];
 
 		$scope.addPost = function(){
 			
 			if(!$scope.title || $scope.title === '') { return; }
-			$scope.posts.push({
-				title: $scope.title, 
-				link: $scope.link,
-				upvotes: 0,
-				 comments: [
-				   {author: 'Joe', body: 'Cool post!', upvotes: 0},
-				   {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-				 ]
-	   		});
+
 
 			$scope.title = '';
 			$scope.link = '';
@@ -49,17 +35,8 @@ angular.module('flapperNews', ['ui.router'])
 			post.upvotes += 1;
 		};
 
-		$scope.addComment = function(){
-		  if($scope.body === '') { return; }
-		  $scope.post.comments.push({
-		    body: $scope.body,
-		    author: 'user',
-		    upvotes: 0
-		  });
-		  $scope.body = '';
-		};
-
 	}
 
 ]);
+
 
