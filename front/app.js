fd9d71b9732344eca0d1feaebed7f6449fc619ca
ChrisLai <chrislai@vt.edu>
2015-01-27 21:30:38 -0500

    Finished Posts Page

--- front/app.js
+++ front/app.js
@@ -6,12 +6,17 @@ flapperNews.config([
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
+    $stateProvider.state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
     });
+
+	$stateProvider.state('posts', {
+	  url: '/posts/{id}',
+	  templateUrl: '/posts.html',
+	  controller: 'PostsCtrl'
+	});
 
   $urlRouterProvider.otherwise('home');
 }]);
@@ -24,6 +29,24 @@ flapperNews.factory('posts', [function(){
   return o;
 }]);
 
+flapperNews.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function(){
+	  if($scope.body === '') { return; }
+	  $scope.post.comments.push({
+	    body: $scope.body,
+	    author: 'user',
+	    upvotes: 0
+	  });
+	  $scope.body = '';
+	};
+	
+}]);
 
 flapperNews.controller('MainCtrl', [
 '$scope',
@@ -43,6 +66,17 @@ function($scope, posts){
 	  });
 	  $scope.title = '';
 	  $scope.link = '';
+
+	  $scope.posts.push({
+		  title: $scope.title,
+		  link: $scope.link,
+		  upvotes: 0,
+		  comments: [
+		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		  ]
+		});
+
 	};
 
 	$scope.incrementUpvotes = function(post) {
