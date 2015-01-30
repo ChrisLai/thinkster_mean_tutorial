93d5e0c70f50bb282d969bde15a18a11b2ef44c9
chrislai <chrislai@vt.edu>
2015-01-30 10:54:42 -0500

    Working on Finishing off with comments in Opening REST routes

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -21,6 +21,10 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
 /* POST post */
 router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
@@ -32,5 +36,26 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+/* Upvote */
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+/* PARAM post */
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post")); }
+
+    req.post = post;
+    return next();
+  });
+});
 
 module.exports = router;
